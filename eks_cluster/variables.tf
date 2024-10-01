6b791bdfec9024f2e58e95124ba3e91dff6aec29
sairam reddy <kasireddysairam03@gmail.com>
2024-10-01 17:18:14 +0530

    adding eks &jenkins files to repo

--- eks_cluster/variables.tf
+++ eks_cluster/variables.tf
@@ -2,24 +2,24 @@
 variable "vpc_cidr" {
   description = "Vpc CIDR"
   type        = string
-  default = "192.168.0.0/16"
+  default     = "192.0.0.0/16"
 }
 
 variable "public_subnets" {
   description = "public_subnets CIDR"
   type        = list(string)
-  default =  ["192.0.1.0/24", "192.0.2.0/24", "192.0.3.0/24"]
+  default     = ["192.0.1.0/24", "192.0.2.0/24", "192.0.3.0/24"]
 }
 
 variable "private_subnets" {
   description = "public_subnets CIDR"
   type        = list(string)
-  default = ["192.0.4.0/24", "192.0.5.0/24", "192.0.6.0/24"]
+  default     = ["192.0.4.0/24", "192.0.5.0/24", "192.0.6.0/24"]
 
 }
 
 variable "instance_types" {
   description = "Instance Type"
   type        = list(string)
-  default = ["t2.medium"]
+  default     = ["t2.medium"]
 }
